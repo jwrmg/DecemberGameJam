0941928d9cd0a94e4f0f5d205794cc96191f7c1d
TraighWarrenAIE <s191299@students.aie.edu.au>
2021-12-10 14:59:08 +1030

    I made attempt

---
+++ GameJam/GameJam/Assets/Scripts/TutorialFunctionality.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialFunctionality : MonoBehaviour
+{
+    public GameObject tutMenuUI;
+    void Start()
+    {
+        Time.timeScale = 0f;
+        tutMenuUI.SetActive(true);
+
+    }
+    void Update()
+    {
+        
+    }
+}
